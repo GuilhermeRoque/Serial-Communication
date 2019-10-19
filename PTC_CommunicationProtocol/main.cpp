4a8edfaf6e869007704c7a95ed7d37d05e857a34
Vinicius Souza <viniciusls22@gmail.com>
2019-10-19 20:35:26 -0300

    Adiciona opcao para inicializar com callback tun no main
    Signed-off-by: Vinicius Souza <viniciusls22@gmail.com>

--- PTC_CommunicationProtocol/main.cpp
+++ PTC_CommunicationProtocol/main.cpp
@@ -12,10 +12,12 @@ using namespace std;
 
 int main(int argc, char ** argv) {
 	uint8_t id_sessao;
-	if(argc < 3){
+	if(argc != 3 && argc != 5){
 		printf("Utilize a seguinte sintaxe para execucao:\n");
-		printf("./PTC_CommunicationProtocol <FD_serial> <id_sessao>\n");
-		printf("Ex.: ./PTC_CommunicationProtocol /dev/pts/3 55\n");
+		printf("./PTC_CommunicationProtocol <FD_serial> <id_sessao> <IP_origem> <IP_destino>\n");
+		printf("Ex.: ./PTC_CommunicationProtocol /dev/pts/3 55 10.10.10.1 10.10.10.2\n\n\n");
+
+		printf("Obs.: caso nao seja informado o IP de origem e destino, sera inicializada a fake layer no lugar da TUN.\n");
 		return -1;
 	}
 
@@ -32,28 +34,34 @@ int main(int argc, char ** argv) {
     Framming framming(rf, 1026, 1000); //agr tem mais 2 bytes de ctrl então é 1026
     ARQ arq(TIMEOUT_ACK, id_sessao);
     Session sessao(-1, TIMEOUT_SESSION, id_sessao);
-    App app(0,5000);
-//    Tun tun("ptc_iface", "10.10.10.2", "10.10.10.1");
-//    tun.start();
-//    CallbackTun ctun(tun, 1000);
 
     framming.set_upper(&arq);
     arq.set_lower(&framming);
     arq.set_upper(&sessao);
     sessao.set_lower(&arq);
-    sessao.set_upper(&app);
-    app.set_lower(&sessao);
-//    sessao.set_upper(&ctun);
-//    ctun.set_lower(&sessao);
-//    ctun.init();
 
     Poller sched;
     sched.adiciona(&framming);
     sched.adiciona(&arq);
     sched.adiciona(&sessao);
-    sched.adiciona(&app);
-//    sched.adiciona(&ctun);
+
+    if (argc == 3) {
+    	App app(0,5000);
+    	sessao.set_upper(&app);
+    	app.set_lower(&sessao);
+    	sched.adiciona(&app);
+    	app.init();
+    } else {
+		Tun tun("ptc_iface", argv[3], argv[4]);
+		tun.start();
+		CallbackTun ctun(tun, 5000);
+		sessao.set_upper(&ctun);
+		ctun.set_lower(&sessao);
+		sched.adiciona(&ctun);
+		ctun.init();
+    }
+
     sched.despache();
-    
+
     return 0;
 }
